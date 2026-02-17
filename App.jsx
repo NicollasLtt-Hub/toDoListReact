ff942e285b86b451a2323dc9f10c846f5277e95b
Nicollas Lott <nicollaslottcurvellano@outlook.com>
2026-02-16 21:42:52 -0300

    Initial project setup with Vite, cleaned up the structure, and applied basic styling to the app.

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+import './App.css';
+
+export function App() {
+  return (
+    <div className='container'>
+      <h1 className='app-title'>Lista de Tarefas</h1>
+    </div>
+  );
+}
